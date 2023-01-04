c647f507be37fe29c8f791f2a60fdf2bc0f36637
AnatoliiZh <abcland2014@gmail.com>
2023-01-04 10:27:59 +0200

    add gradient and heart

--- src/components/Articles/ArticlesListItem.tsx
+++ src/components/Articles/ArticlesListItem.tsx
@@ -5,6 +5,10 @@ import './ArticlesListItem.scss'
 // import Logo from 'components/Logo/Logo'
 // import Image from 'components/Image/Image'
 // import img1 from 'assets/1.webp'
+// import Box from '@mui/material/Box'
+import { FaRegClock } from 'react-icons/fa'
+import { FaRegCommentAlt } from 'react-icons/fa'
+import { FaRegHeart } from 'react-icons/fa'
 
 type Props = {
     title: string
@@ -26,9 +30,11 @@ const ArticlesListItem = ({
             className="article"
             variant="outlined"
             sx={{
-                backgroundImage: `url(${image})`,
+                backgroundImage: `linear-gradient(to top, black, 10%, transparent 20%), url(${image})`,
                 backgroundSize: 'cover',
                 backgroundPositionY: '0px',
+                border: 'none',
+                borderRadius: '0px',
             }}
         >
             <CardContent
@@ -37,10 +43,18 @@ const ArticlesListItem = ({
                 }}
             >
                 <div className="article-title">{title}</div>
-                <div className="cat-date-com">
-                    <div className="article-category">{category}</div>
-                    <div className="article-date">{date}</div>
-                    <div className="article-comments">{comments}</div>
+                <div className="under-title">
+                    <div className="cat-date-com">
+                        {/* <div className="article-category">{category}</div> */}
+                        <a href="#" className="article-category">
+                            {category}
+                        </a>
+                        <FaRegClock className="icons" />
+                        <div className="article-date">{date}</div>
+                        <FaRegCommentAlt className="icons" />
+                        <div className="article-comments">{comments}</div>
+                    </div>
+                    <FaRegHeart className="icons" />
                 </div>
             </CardContent>
         </Card>
