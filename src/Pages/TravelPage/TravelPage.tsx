03269711858ed0f8c29b359e95d5fbf2f6802a2a
AnatoliiZh <abcland2014@gmail.com>
2023-01-15 15:06:01 +0200

    add information to TravelPage

--- src/Pages/TravelPage/TravelPage.tsx
+++ src/Pages/TravelPage/TravelPage.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Container } from '@mui/material'
+import { Grid } from '@mui/material'
 import 'Pages/Pages.scss'
+import ArticlesInOneCategory from 'components/ArticlesInOneCategory/ArticlesInOneCategory'
+import Search from 'components/Search/Search'
+import Categories from 'components/Categories/Categories'
+import SocialProfiles from 'components/SocialProfiles/SocialProfiles'
 
 type Props = {}
 
@@ -8,6 +13,16 @@ const TravelPage = (props: Props) => {
     return (
         <Container maxWidth="lg">
             <div className="title">Category : Travel</div>
+            <Grid container spacing={6}>
+                <Grid item xs={8}>
+                    <ArticlesInOneCategory category="Travel" />
+                </Grid>
+                <Grid item xs={4}>
+                    <Search />
+                    <Categories />
+                    <SocialProfiles />
+                </Grid>
+            </Grid>
         </Container>
     )
 }
