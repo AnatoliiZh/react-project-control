3c92abd54279c331d7276d97a695418038dab5fa
AnatoliiZh <abcland2014@gmail.com>
2023-01-15 14:32:30 +0200

    add information to LifestylePage

--- src/Pages/LifestylePage/LifestylePage.tsx
+++ src/Pages/LifestylePage/LifestylePage.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Container } from '@mui/material'
+import { Grid } from '@mui/material'
 import 'Pages/Pages.scss'
+import ArticlesInOneCategory from 'components/ArticlesInOneCategory/ArticlesInOneCategory'
+import Search from 'components/Search/Search'
+import Categories from 'components/Categories/Categories'
+import SocialProfiles from 'components/SocialProfiles/SocialProfiles'
 
 type Props = {}
 
@@ -8,7 +13,18 @@ const LifestylePage = (props: Props) => {
     return (
         <Container maxWidth="lg">
             <div className="title">Category : Lifestyle</div>
+            <Grid container spacing={6}>
+                <Grid item xs={8}>
+                    <ArticlesInOneCategory category="Lifestyle" />
+                </Grid>
+                <Grid item xs={4}>
+                    <Search />
+                    <Categories />
+                    <SocialProfiles />
+                </Grid>
+            </Grid>
         </Container>
+
     )
 }
 
