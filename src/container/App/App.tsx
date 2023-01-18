058448833a84addda15632b5b5cda95e4acfa170
AnatoliiZh <abcland2014@gmail.com>
2023-01-18 13:18:30 +0200

    begining like

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -3,9 +3,28 @@ import Header from '../Header/Header'
 import { StyledEngineProvider } from '@mui/material/styles'
 import Main from 'container/Main/Main'
 import Footer from 'container/Footer/Footer'
+import { useState } from 'react'
+
 
 type Props = {}
+
+type ArticlesLikeState = {
+    [id: number]: boolean
+}
+
 const App = (props: Props) => {
+    const [articlesLikeState, setArticlesLikeState] =    
+        useState<ArticlesLikeState>({
+            1: true,
+            2: true,
+        })
+
+    const changeLike = (id: number) => {
+        setArticlesLikeState((prevState: ArticlesLikeState) => ({
+            ...prevState,
+            [id]: !prevState[id],
+        }))
+    }
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
