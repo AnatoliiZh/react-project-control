660bedf4b80c915072e96eed28b341f4db747f62
AnatoliiZh <abcland2014@gmail.com>
2023-01-26 11:21:04 +0200

    improve quantity of category with Object.keys(arrCategory).map

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -6,100 +6,49 @@ import './Categories.scss'
 type Props = {}
 
 const Categories = (props: Props) => {
-    let arrCategory = new Map();   
-    for ( let key in articlesArray) {        
-        if (arrCategory.has(articlesArray[key].category))  {
-            arrCategory.set(articlesArray[key].category,arrCategory.get(articlesArray[key].category)+1)
+    type ArrCategoryType = {
+        [key: string]: number
+    }
+    const arrCategory: ArrCategoryType = {}
+    for (let key in articlesArray) {
+        if (arrCategory.hasOwnProperty(articlesArray[key].category)) {
+            arrCategory[articlesArray[key].category] =
+                arrCategory[articlesArray[key].category] + 1
         } else {
-            arrCategory.set(articlesArray[key].category,1)
-        }      
-        //  console.log( "Ключ: " + key + " значение: " + articlesArray[key].category );
-      };
-    //   console.log(arrCategory);
+            arrCategory[articlesArray[key].category] = 1
+        }
+    }
+
+
+      
+    // for (let key1 in arrCategory) {
+    //     console.log(arrCategory[key1])
+        
+    // }
+    // console.log(arrCategory)
+    // console.log(Object.keys(arrCategory))
+    // console.log(Object.keys(arrCategory)[0])
+    // console.log(arrCategory[Object.keys(arrCategory)[0]])
+
+    
+
     return (
         <div className="categories">
             <div className="categories-heading">
                 <div className="categories-title">Categories</div>
                 <div className="categories-line"></div>
             </div>
-            {/* <ul>
-                {Object.fromEntries(arrCategory).map((value:string, index:number) => <li key={index}>{value}</li>)}
-            </ul> */}
             
-
-           
-             
-             <div className="categories-box">
-                 <div className="categories-name">
-                     <LinkItem to={`/Fashion`}>Fashion</LinkItem>
-                 </div>
-                 <div className="categories-quantity">
-                     <span>
-                         {
-                             articlesArray.filter(
-                                 (filtered) => filtered.category === 'Fashion'
-                             ).length
-                         }
-                     </span>
-                 </div>
-             </div>
-
-             <div className="categories-box">
-                 <div className="categories-name">
-                     <LinkItem to={`/Fashion`}>Fashion</LinkItem>
-                 </div>
-                 <div className="categories-quantity">
-                     <span>
-                         {
-                             articlesArray.filter(
-                                 (filtered) => filtered.category === 'Fashion'
-                             ).length
-                         }
-                     </span>
-                 </div>
-             </div>
-             <div className="categories-box">
-                 <div className="categories-name">
-                     <LinkItem to={`/Health`}>Health</LinkItem>
-                 </div>
-                 <div className="categories-quantity">
-                     <span>
-                         {
-                             articlesArray.filter(
-                                 (filtered) => filtered.category === 'Health'
-                             ).length
-                         }
-                     </span>
-                 </div>
-             </div>
-             <div className="categories-box">
-                 <div className="categories-name">
-                     <LinkItem to={`/Lifestyle`}>Lifestyle</LinkItem>
-                 </div>
-                 <div className="categories-quantity">
-                     <span>
-                         {
-                             articlesArray.filter(
-                                 (filtered) => filtered.category === 'Lifestyle'
-                             ).length
-                         }
-                     </span>
-                 </div>
-             </div>
-             <div className="categories-box">
-                 <div className="categories-name">
-                     <LinkItem to={`/Travel`}>Travel</LinkItem>
-                 </div>
-                 <div className="categories-quantity">
-                     <span>
-                         {
-                             articlesArray.filter(
-                                 (filtered) => filtered.category === 'Travel'
-                             ).length
-                         }
-                     </span>
-                 </div>
-             </div>
+            {Object.keys(arrCategory).map((item)  => ( 
+                <div className="categories-box">
+                <div className="categories-name">
+                    <LinkItem to={`/${item}`}>{item}</LinkItem>
+                </div>
+                <div className="categories-quantity">
+                    <span>{arrCategory[item]} </span>
+                </div>
+                </div>
+            ))}
         </div>
     )
 }
