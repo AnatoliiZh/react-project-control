5064a9d2045f253f3fe4eecc1d42a34e79360bbc
AnatoliiZh <abcland2014@gmail.com>
2023-01-31 17:25:27 +0200

    done pagination to BlogPage

--- src/Pages/BlogPage/BlogPage.tsx
+++ src/Pages/BlogPage/BlogPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import ArticlesListItem from 'components/Articles/ArticlesListItem'
 import articlesArray from 'utils/articlesArray'
 import { Container, Grid } from '@mui/material'
@@ -6,6 +6,8 @@ import 'Pages/Pages.scss'
 import ButtonToTop from 'components/ButtonToTop/ButtonToTop'
 import Pagination from '@mui/material/Pagination'
 import Stack from '@mui/material/Stack'
+
+
 
 type ArticleProps = {
     id: number
@@ -16,22 +18,13 @@ type ArticleProps = {
     image: string
 }
 
-type TypeSearch = {
-    text: string
-}
-
-// type Props = {
-//     changeLike: (id: number) => void
-//     articlesLikeState: ArticlesLikeState
-// }
-
 
 const BlogPage = () => {
     useEffect(() => {
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
     }, [])
 
-    const [startId, setStartId] = useState(1)
+   
     const articlesPerPage = 6
     const arrPag = []
     if (articlesArray.length / articlesPerPage > 1) {
@@ -46,25 +39,8 @@ const BlogPage = () => {
 
     const [page, setPage] = React.useState(1)
     const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        setPage(value)
-        // setStartId((page - 1) * 6 + 1)
+        setPage(value)       
     }
-
-    console.log(startId)
-
-    // const [query, setQuery] = useState('')
-
-    // const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     const enteredName = event.target.value
-    //     setQuery(enteredName)
-    // }
-
-    // const search = () => {
-    //     //   const foundItems = PRODUCTS.filter((item) =>
-    //     //     item.name.toLowerCase().includes(query.toLowerCase())
-    //     //   );
-    //     //   setResult(foundItems);
-    // }
 
     return (
         <Container maxWidth="lg">
@@ -109,49 +85,27 @@ const BlogPage = () => {
                                     comments={comments}
                                     image={image}
 
-                                    // changeLike={changeLike}
-                                    // articlesLikeState={articlesLikeState}
                                 />
                             </Grid>
                         )
                     )}
             </Grid>
-            {/* <div className="pagination">
-                {articlesArray.length / articlesPerPage > 1 && (
-                    <div>
-                        {arrPag.map((item) => (
-                            <button
-                                className="btn-pagination"
-                                key={item}
-                                onClick={() => setStartId((item - 1) * 6 + 1)}
-                            >
-                                {item}
-                            </button>
-                        ))}
-                    </div>
-                )}
-            </div> */}
+            
             <ButtonToTop />
-            {/* <div className="wrapper">
-                <input
-                    value={query}
-                    onChange={inputHandler}
-                    placeholder="Search products"
-                    className="input"
-                />
-
-                <button onClick={search}>Search</button>
-            </div> */}
+            
             <Stack spacing={2}>
                 <div className="pagination">
                     <Pagination
                         count={articlesArray.length % articlesPerPage}
                         page={page}
                         onChange={handleChange}
+                        size="large"
+                        
                     />
                 </div>
             </Stack>
         </Container>
+        
     )
 }
 
