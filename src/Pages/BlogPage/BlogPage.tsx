69f8412c234b5ac8a4d5397654926dfbea2c2078
AnatoliiZh <abcland2014@gmail.com>
2023-01-31 12:14:17 +0200

    begin

--- src/Pages/BlogPage/BlogPage.tsx
+++ src/Pages/BlogPage/BlogPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ArticlesListItem from 'components/Articles/ArticlesListItem'
 import articlesArray from 'utils/articlesArray'
 import { Container, Grid } from '@mui/material'
@@ -25,48 +25,77 @@ type ArticleProps = {
 
 const BlogPage = () => {
     useEffect(() => {
-        // 👇️ scroll to top on page load
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
     }, [])
-  return (
-    <Container maxWidth='lg'>
-        <div className="title">Blog</div>
-        
-    <Grid
-        container
-        direction="row"
-        justifyContent="space-around"
-        alignItems="center"
-        spacing={4}
-    >
-        {articlesArray.map(
-                ({
-                    id,
-                    title,
-                    category,
-                    date,
-                    comments,
-                    image,
-                }: ArticleProps) => (
-                    <Grid item xs={12} sm={4} md={4} key={id}>
-                        
-                        <ArticlesListItem
-                            id={id}
-                            title={title}
-                            category={category}
-                            date={date}
-                            comments={comments}
-                            image={image}
-                            // changeLike={changeLike}
-                            // articlesLikeState={articlesLikeState}
-                        />
-                    </Grid>
-                )
-            )}
-    </Grid>
-    <ButtonToTop/>
-    </Container>
-  )
+
+    const [startId, setStartId] = useState(1)
+    const articlesPerPage = 6
+    const arrPag = []
+    if (articlesArray.length / articlesPerPage > 1) {
+        for (
+            let index = 0;
+            index < articlesArray.length % articlesPerPage;
+            index++
+        ) {
+            arrPag[index] = index + 1
+        }
+    }
+
+    return (
+        <Container maxWidth="lg">
+            <div className="title">Blog</div>
+
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+                spacing={4}
+            >
+                {articlesArray
+                    .filter(
+                        (filtered) =>
+                            filtered.category
+                                .toLowerCase()
+                                .indexOf('fa'.toLowerCase()) !== -1
+                    )
+                    .map(
+                        // {articlesArray.map(
+                        ({
+                            id,
+                            title,
+                            category,
+                            date,
+                            comments,
+                            image,
+                        }: ArticleProps) => (
+                            <Grid item xs={12} sm={4} md={4} key={id}>
+                                <ArticlesListItem
+                                    id={id}
+                                    title={title}
+                                    category={category}
+                                    date={date}
+                                    comments={comments}
+                                    image={image}
+                                    // changeLike={changeLike}
+                                    // articlesLikeState={articlesLikeState}
+                                />
+                            </Grid>
+                        )
+                    )}
+            </Grid>
+            <div className="pagination">
+                {articlesArray.length / articlesPerPage > 1 && (
+                    <div>
+                        {arrPag.map((item) => (
+                            <button className="btn-pagination">{item}</button>
+                        ))}
+                    </div>
+                )}
+            </div>
+            <ButtonToTop />
+        </Container>
+    )
 }
 
 export default BlogPage
